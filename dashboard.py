c17c53487714de80088d230f041370fc4a0fcb79
MUHAMMAD FIRDAUS BIN MD LANI <chrishyungg97@gmail.com>
2026-09-23 02:00:24 +0800

    Update Dashboard for Cloud PostgreSQL & added requirements.txt

--- dashboard.py
+++ dashboard.py
@@ -1,34 +1,36 @@
 import streamlit as st
 import pandas as pd
-import sqlite3
+from sqlalchemy import create_engine
 import plotly.express as px
+import os
+from dotenv import load_dotenv
 
-# 1. Page Configuration
+# Load environment variables invisibly
+load_dotenv()
+
 st.set_page_config(page_title="Weather ETL Dashboard", layout="wide")
 st.title("🌦️ Global Weather Dashboard")
-st.markdown("This dashboard reads directly from the local SQLite database updated daily by the ETL pipeline.")
+st.markdown("This dashboard reads directly from a Neon Cloud PostgreSQL database updated daily by a GitHub Actions ETL pipeline.")
 
-# 2. Connect to Database & Load Data
-DB_FILE = r"C:\Users\firdaus\Documents\Automated-Weather-ETL-Pipeline\weather_data.db"
+# Grab the cloud database password
+DB_URL = os.getenv("DB_URL")
 
-@st.cache_data # This prevents Streamlit from querying the DB every time you click a button
+@st.cache_data
 def load_data():
-    conn = sqlite3.connect(DB_FILE)
-    df = pd.read_sql("SELECT * FROM daily_weather", conn)
-    conn.close()
+    engine = create_engine(DB_URL)
+    df = pd.read_sql("SELECT * FROM daily_weather", engine)
     return df
 
-df = load_data()
+try:
+    df = load_data()
+    if not df.empty:
+        st.subheader("Temperature Trends by City")
+        fig_temp = px.line(df, x="observation_time", y="temperature_c", color="city", markers=True)
+        st.plotly_chart(fig_temp, use_container_width=True)
 
-# 3. Build Visualizations
-if not df.empty:
-    # Create a multi-line chart comparing temperatures
-    st.subheader("Temperature Trends by City")
-    fig_temp = px.line(df, x="observation_time", y="temperature_c", color="city", markers=True)
-    st.plotly_chart(fig_temp, use_container_width=True)
-
-    # Show the raw database table at the bottom
-    st.subheader("Raw Database Records")
-    st.dataframe(df.sort_values(by="observation_time", ascending=False))
-else:
-    st.warning("The database is currently empty. Run your ETL script first!")
+        st.subheader("Raw Database Records")
+        st.dataframe(df.sort_values(by="observation_time", ascending=False))
+    else:
+        st.warning("The database is currently empty.")
+except Exception as e:
+    st.error(f"Database connection failed. Did you set the DB_URL secret?")
