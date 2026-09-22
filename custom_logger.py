97069ff67d4dee7c6126950737971877727812ae
MUHAMMAD FIRDAUS BIN MD LANI <chrishyungg97@gmail.com>
2026-09-23 01:56:34 +0800

    Fix workflow to use GitHub Secret

--- custom_logger.py
+++ custom_logger.py
@@ -1,30 +1,25 @@
 import logging
 import os
-import sys  # Required for terminal output
+import sys 
 
 def get_pipeline_logger():
-    # 1. Define paths
-    BASE_DIR = r"C:\Users\firdaus\Documents\Automated-Weather-ETL-Pipeline"
+    # Dynamically get the current folder (works on Windows, Mac, and Linux)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     LOG_FILE = os.path.join(BASE_DIR, "pipeline.log")
 
-    # 2. Create a custom logger object
     logger = logging.getLogger("WeatherETL")
     logger.setLevel(logging.INFO)
 
-    # 3. Prevent duplicate log entries
     if not logger.handlers:
-        # Create the formatter we want to use for both handlers
         formatter = logging.Formatter(
             "%(asctime)s - %(levelname)s - %(message)s", 
             datefmt="%Y-%m-%d %H:%M:%S"
         )
         
-        # Handler 1: Writes to pipeline.log
         file_handler = logging.FileHandler(LOG_FILE)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
-        # Handler 2: Prints to the VS Code Terminal
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
